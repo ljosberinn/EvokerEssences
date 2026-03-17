301eb2a3a185b2e96c8b820360a625b34f9241a7
Gerrit Alex <admin@gerritalex.de>
2026-03-17 17:44:11 +0100

    patttern match vibe some settings in here

---
+++ Enum.lua
@@ -0,0 +1,16 @@
+---@type string, Essences
+local _, Private = ...
+
+---@class EssencesEnums
+Private.Enum = {}
+
+---@enum EssencesEvents
+Private.Enum.Events = {
+	SETTING_CHANGED = "SETTING_CHANGED",
+}
+
+---@enum Preset
+Private.Enum.Preset = {
+	Xeph = 1,
+	Vollmer = 2,
+}
